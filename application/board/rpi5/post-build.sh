5b2abe70f9599e9b6dd15c4fabbc8a7f01d4b49a
Azad Karatas <azadkaratas@gmail.com>
2025-05-04 17:52:41 +0200

    Enable Wi-Fi at boot up
    Signed-off-by: Azad Karatas <azadkaratas@gmail.com>

--- application/board/rpi5/post-build.sh
+++ application/board/rpi5/post-build.sh
@@ -1,3 +1,7 @@
 
 cp -a ${BR2_EXTERNAL_APPLICATION_PATH}/board/rpi5/overlay/system/* ${TARGET_DIR}/
 cp ${BR2_EXTERNAL_APPLICATION_PATH}/package/qt-menu/S99qtmenu ${TARGET_DIR}/etc/init.d/S99qtmenu
+
+cp package/busybox/S10mdev ${TARGET_DIR}/etc/init.d/S10mdev
+chmod 755 ${TARGET_DIR}/etc/init.d/S10mdev
+cp package/busybox/mdev.conf ${TARGET_DIR}/etc/mdev.conf
